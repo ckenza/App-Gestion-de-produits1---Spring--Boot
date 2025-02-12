6ce8bb7fea9f36267044dd980c857fb4dcbb42c6
ckenza <kenza.chrifi36@gmail.com>
2025-02-12 15:28:59 +0100

    spring boot project initialized, database created, environment variables configured

---
+++ src/main/schema.sql
@@ -0,0 +1,32 @@
+use glam_db;
+
+CREATE TABLE IF NOT EXISTS product(
+	id_product int PRIMARY KEY NOT NULL,
+    title_product VARCHAR(255),
+    image_url VARCHAR(255),
+    price double,
+    stock int
+);
+
+
+CREATE TABLE IF NOT EXISTS user(
+	id_user INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+
+CREATE TABLE IF NOT EXISTS invoice(
+    id_invoice int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    date date,
+    total double
+);
+
+
+CREATE TABLE IF NOT EXISTS invoice_item(
+    id_invoice int,
+    id_product int,
+    quantity int,
+    FOREIGN KEY (id_invoice) REFERENCES invoice(id_invoice),
+    FOREIGN KEY (id_product) REFERENCES product(id_product)
+);
---
+++ src/test/java/com/project/glam_back/GlamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.glam_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
